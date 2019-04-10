442043f68c66bcd1fd7d1d65364e4852ecc36199
Aaron Chu <aaronchu357@gmail.com>
2019-04-10 14:57:37 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,22 @@
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  final = {}
+  data.each do |attribute, hash|
+    hash.each do |description, arr|
+      arr.each do |element|
+        if final.key?(element) == false
+          final[element] = {}
+        end
+
+        if final[element].key?(attribute) == false
+          final[element][attribute] = []
+        end
+
+        if final[element][attribute].include?(description) == false
+          final[element][attribute] << description.to_s
+        end
+
+      end
+    end
+  end
+  final
 end
