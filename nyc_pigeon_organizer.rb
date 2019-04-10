85100df48c801ecdec33431e57b3d7059a3c2d84
Aaron Chu <aaronchu357@gmail.com>
2019-04-10 14:46:01 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,9 +1,15 @@
 def nyc_pigeon_organizer(data)
+  final = {}
   data.each do |attribute, hash|
     hash.each do |description, arr|
       arr.each do |element|
-        return element
+        if final.key?(element) == false
+          final[element] = [attribute]
+        else
+          final[element] << description
+        end
       end
     end
   end
+  final
 end
