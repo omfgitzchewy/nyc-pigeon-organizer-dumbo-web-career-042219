50d12c25afd7a4bb4818a28d94521a9cc738269c
Aaron Chu <aaronchu357@gmail.com>
2019-04-10 14:42:41 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -2,7 +2,7 @@ def nyc_pigeon_organizer(data)
   data.each do |attribute, hash|
     hash.each do |description, arr|
       arr.each do |element|
-        
+        return element
       end
     end
   end
